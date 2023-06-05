565e5966f2acb990219fdb2a6fe5d327c0e5ab69
lelong181 <longld2284@gmail.com>
2023-06-05 22:04:10 +0700

    Commit

---
+++ DemoContentNegotiation/Controllers/ProfileController.cs
@@ -0,0 +1,42 @@
+﻿using DemoContentNegotiation.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata;
+
+namespace DemoContentNegotiation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfileController : ControllerBase{
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Profile> profiles = new List<Profile>();
+            var profile = new Profile
+            {
+                StudentID = Guid.NewGuid(),
+                Name = "long 1",
+                Age = 1
+            };
+            profiles.Add(profile);
+
+            profile = new Profile
+            {
+                StudentID = Guid.NewGuid(),
+                Name = "Long 2",
+                Age = 2
+            };
+            profiles.Add(profile);
+
+            profile = new Profile
+            {
+                StudentID = Guid.NewGuid(),
+                Name = "Long 3",
+                Age = 3
+            };
+            profiles.Add(profile);
+
+            return Ok(profiles);
+        }
+    }
+}
---
+++ DemoContentNegotiation/Models/Profile.cs
@@ -0,0 +1,13 @@
+﻿namespace DemoContentNegotiation.Models
+{
+    public class ProfileB
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class Profile : ProfileB
+    {
+        public Guid StudentID { get; set; }
+    }
+}
